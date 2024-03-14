f4fd74d569f3526b3e501331cbdd9ff1dc090c49
saadProf <saadprof101@gmail.com>
2024-03-14 10:51:04 +0600

    code added for loop_Array&Obj.js

---
+++ loop_Array&Obj.js
@@ -0,0 +1,45 @@
+const urbanLegends = [
+    "Kuchisake-Onna",
+    "Aka Manto",
+    "Tomino",
+    "Hitobashira",
+    "Aisha Kandisha",
+    "The skinwalker",
+    "Hannah Cranna",
+    "La Mala Hora",
+    "White Lady Lane"
+]
+
+// for loop for array
+for(let i = 0; i < urbanLegends.length; i++){
+    // console.log(urbanLegends[i]);
+}
+
+// while loop for array
+let i = 0;
+while(i < urbanLegends.length){
+    // console.log(urbanLegends[i]);
+    i++;
+}
+
+// for...of loop
+for(const legend of urbanLegends){
+    // console.log(legend);
+}
+
+
+
+const legendsByPlace = {
+    Nepal: "The Yeti",
+    India: "Kuldhara Village",
+    UAE: "Jinn",
+    Europe: "Krampus",
+    USA: "Slaughterhouse Canyon",
+    Bangladesh: "Mecho Bhoot",
+    Japan: "Kiyotaki Tunnel"
+}
+
+// for...in loop
+for(const legend in legendsByPlace){
+    console.log(`${legend}: ${legendsByPlace[legend]}`);
+}
