62018558e57f45e777f74d51ac743623c4681a56
saadProf <saadprof101@gmail.com>
2024-03-22 08:43:43 +0600

    Decreamenting system added.

--- calcFactorial.js
+++ calcFactorial.js
@@ -1,10 +1,16 @@
 function factorial(num){
     let result = 1;
-    for(let i = 1; i <= num; i++){
+
+    // for(let i = 1; i <= num; i++){
+    //     result *= i;
+    // }
+
+    for(let i = num; i >= 1; i--){
         result *= i;
     }
+
     return result;
 }
 
-const factorialResult = factorial(3);
+const factorialResult = factorial(5);
 console.log(factorialResult);
