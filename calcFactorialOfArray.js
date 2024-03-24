76773a6f3f1869adf00b139d9a225d04c1b11c31
saadProf <saadprof101@gmail.com>
2024-03-24 09:30:01 +0600

    Found a bug. the factorial calculator wasn't working. Now it is fixed. Also now this function will return result in a object.

--- calcFactorialOfArray.js
+++ calcFactorialOfArray.js
@@ -1,20 +1,20 @@
-const factorialArray = [ "Hi", 6, 5, 10, 17, 31, 23, 71, 54 ]
+const factorialArray = [ "Hi", 6, 5, 10, 17, 31, 23, undefined, 54 ]
 
 function factorialOfArray(arrName){
     
-    let factorial = 1;
     let result = [];
     let faulties = [];
-
+    
     if(!Array.isArray(arrName)) return "Invalid input";
     if(Array.isArray(arrName)){
         for(const arrItem of arrName){
             if(typeof arrItem !== "number"){
                 faulties.push(arrItem);
             }
-
+            
             if(typeof arrItem === "number" && arrItem > 0){
-                for(let i = arrItem; i >= 1; i++){
+                let factorial = 1;
+                for(let i = 1; i <= arrItem; i++){
                     factorial *= i;
                 }
                 result.push(factorial);
@@ -22,7 +22,8 @@ function factorialOfArray(arrName){
         }
     }
 
-    console.log(result, faulties);
+    return {result, faulties};
 }
 
-factorialOfArray(factorialArray);
+const factorialResultArr = factorialOfArray(factorialArray);
+console.log(factorialResultArr);
