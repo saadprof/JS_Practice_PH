b049a06ec1e5cf63f1a6407ef9abb774943e9a22
saadProf <saadprof101@gmail.com>
2024-03-26 09:42:30 +0600

    This function can return the highest value of an array.

---
+++ maxOfArray.js
@@ -0,0 +1,17 @@
+const numbers = [ 73, 45, 51, 78, 92, 89, 33, 31, 19, 49 ];
+
+function maxOfArray(arrName){
+    let max = arrName[0];
+
+    for(let i = 0; i < numbers.length; i++){
+        if(arrName[i] > max){
+            max = arrName[i];
+        }
+    }
+
+    return max;
+
+}
+
+const maxResult = maxOfArray(numbers);
+console.log(maxResult);
