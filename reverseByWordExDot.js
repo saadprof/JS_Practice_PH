c6160d026f6a30fac206706feca35fdc046cb455
saadProf <saadprof101@gmail.com>
2024-03-27 19:46:43 +0600

    poped the dot from last sentence, joined them together to form the same sentence without dot

--- reverseByWordExDot.js
+++ reverseByWordExDot.js
@@ -4,10 +4,17 @@ function reverseByWordExDot(sentence){
     // At first we have to split sentences by dot.
     const splitedByDot = sentence.split('. ');
     
+    // Then we take the last sentence containing dot and split it by alphabets.
     const lastSentence = splitedByDot[splitedByDot.length - 1];
     const splitLastSentence = lastSentence.split('');
+    
+    // Now we exclude the dot from the splited sentence.
+    splitLastSentence.pop();
 
-    console.log(splitLastSentence);
+    // And to we join them together to create the same sentence without dot.
+    const lastSentenceExDot = splitLastSentence.join('');
+    
+    console.log(lastSentenceExDot);
 }
 
 reverseByWordExDot(dumSentence);
