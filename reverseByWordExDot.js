d4d0f5919cc1d864103b0313c65ebef151b0a1d6
saadProf <saadprof101@gmail.com>
2024-03-27 19:34:29 +0600

    Now we take the last sentence with dot and splitting it to exclude the dot

--- reverseByWordExDot.js
+++ reverseByWordExDot.js
@@ -1,10 +1,13 @@
 const dumSentence = "Hi, I'm a dummey sentence. You might use me. And yes, I've some dots on me.";
 
 function reverseByWordExDot(sentence){
+    // At first we have to split sentences by dot.
     const splitedByDot = sentence.split('. ');
+    
+    const lastSentence = splitedByDot[splitedByDot.length - 1];
+    const splitLastSentence = lastSentence.split('');
 
-    console.log(splitedByDot);
-
+    console.log(splitLastSentence);
 }
 
 reverseByWordExDot(dumSentence);
