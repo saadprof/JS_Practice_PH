f729e41e9f2679da48ef0be361f84231520ed016
saadProf <saadprof101@gmail.com>
2024-03-29 14:09:01 +0600

    Array of objects about phone spec file added to genFile for moduler use.

---
+++ genFile/genPhoneArrObj.js
@@ -0,0 +1,123 @@
+const phoneSpec = [
+    {
+      "id": 1,
+      "phone_name": "iPhone",
+      "phone_model": "iPhone 13",
+      "phone_price_bdt": 87900
+    },
+    {
+      "id": 2,
+      "phone_name": "Samsung",
+      "phone_model": "Galaxy S21",
+      "phone_price_bdt": 65900
+    },
+    {
+      "id": 3,
+      "phone_name": "Google",
+      "phone_model": "Pixel 6",
+      "phone_price_bdt": 54900
+    },
+    {
+      "id": 4,
+      "phone_name": "OnePlus",
+      "phone_model": "OnePlus 9 Pro",
+      "phone_price_bdt": 76900
+    },
+    {
+      "id": 5,
+      "phone_name": "Xiaomi",
+      "phone_model": "Mi 11",
+      "phone_price_bdt": 49900
+    },
+    {
+      "id": 6,
+      "phone_name": "OPPO",
+      "phone_model": "Find X3 Pro",
+      "phone_price_bdt": 79900
+    },
+    {
+      "id": 7,
+      "phone_name": "Realme",
+      "phone_model": "Realme GT",
+      "phone_price_bdt": 54900
+    },
+    {
+      "id": 8,
+      "phone_name": "Huawei",
+      "phone_model": "Mate 40 Pro",
+      "phone_price_bdt": 97900
+    },
+    {
+      "id": 9,
+      "phone_name": "Vivo",
+      "phone_model": "X60 Pro+",
+      "phone_price_bdt": 89900
+    },
+    {
+      "id": 10,
+      "phone_name": "Sony",
+      "phone_model": "Xperia 1 III",
+      "phone_price_bdt": 109900
+    },
+    {
+      "id": 11,
+      "phone_name": "Nokia",
+      "phone_model": "X20",
+      "phone_price_bdt": 34900
+    },
+    {
+      "id": 12,
+      "phone_name": "Motorola",
+      "phone_model": "Edge 20 Pro",
+      "phone_price_bdt": 71900
+    },
+    {
+      "id": 13,
+      "phone_name": "Asus",
+      "phone_model": "ROG Phone 5",
+      "phone_price_bdt": 89900
+    },
+    {
+      "id": 14,
+      "phone_name": "BlackBerry",
+      "phone_model": "KEY2",
+      "phone_price_bdt": 59900
+    },
+    {
+      "id": 15,
+      "phone_name": "Lenovo",
+      "phone_model": "Legion Phone Duel 2",
+      "phone_price_bdt": 89900
+    },
+    {
+      "id": 16,
+      "phone_name": "HTC",
+      "phone_model": "U12+",
+      "phone_price_bdt": 59900
+    },
+    {
+      "id": 17,
+      "phone_name": "LG",
+      "phone_model": "Wing",
+      "phone_price_bdt": 89900
+    },
+    {
+      "id": 18,
+      "phone_name": "ZTE",
+      "phone_model": "Axon 30 Ultra",
+      "phone_price_bdt": 69900
+    },
+    {
+      "id": 19,
+      "phone_name": "Meizu",
+      "phone_model": "18",
+      "phone_price_bdt": 67900
+    },
+    {
+      "id": 20,
+      "phone_name": "Tecno",
+      "phone_model": "Phantom X",
+      "phone_price_bdt": 35900
+    }
+  ]
+  
