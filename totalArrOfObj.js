ce6e3cdbe6d01f84b9851e772e9e1bd2d3ef15f6
saadProf <saadprof101@gmail.com>
2024-03-31 04:05:23 +0600

    importing dummy product array and picking random products from array to get total cost.

---
+++ totalArrOfObj.js
@@ -0,0 +1,13 @@
+import { productSpec } from './genFile/genProductArrObj.js';
+/*
+    At first, we have to import the product array.
+    Then we will create a randomiser function that pick 1-8 number as product quantity.
+    The randomiser function will also pick random products from array previous randomised times.
+*/ 
+
+// Randomiser and product picking function.
+function pickRandomProducts(productArr){
+    console.log(productArr);
+}
+
+pickRandomProducts(productSpec);
